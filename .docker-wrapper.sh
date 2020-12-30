7940be1d58369858ecc617fd689f1a2748be8f74
shun <shun.fix9@gmail.com>
2020-12-30 04:33:28 +0000

    production ready!

--- .docker-wrapper.sh
+++ .docker-wrapper.sh
@@ -1,7 +1,10 @@
 DOCKER_WRAPPER_IMAGE_theia(){ cat $APP_ROOT/.getto-hangar-image; }
 
-DOCKER_WRAPPER_IDE(){
+DOCKER_WRAPPER_APP(){
   theia-server $1 yarn start $APP_ROOT --hostname 0.0.0.0 --port 8080 --ssl --cert $TLS_CERT --certkey $TLS_KEY
+}
+DOCKER_WRAPPER_LOGS(){
+  theia-server logs
 }
 
 DOCKER_WRAPPER_SERVER_OPTS_theia(){ echo "-p 10099:8080"; }
